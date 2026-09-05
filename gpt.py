29c9629c6c3a07159a9773d08a219ba4f35ab230
Michael <michaelcrowley931@gmail.com>
2026-09-05 15:13:12 +0100

    Fixed an issue causing extrememly slow training (I had forgot that attention heads have different dimensions to the rest of the model

--- gpt.py
+++ gpt.py
@@ -4,6 +4,7 @@ from datasets import load_dataset
 import einops
 from huggingface_hub import hf_hub_download
 from tqdm import tqdm
+import time
 
 
 class Config:
@@ -12,39 +13,37 @@ class Config:
         self.batch_size = 16
         self.device = 'mps'
         self.nheads = 8
-        self.dheads = 64
-        self.d_model = self.nheads * self.dheads
+        self.d_heads = 64
+        self.d_model = self.nheads * self.d_heads
         self.vocab = 8562
         self.nlayers = 6
-        self.train_steps = 1000
+        self.train_steps = 10000
     
 class Head(torch.nn.Module):
     def __init__(self, cfg):
         super().__init__()
         self.cfg = cfg
         self.W_pos = torch.nn.Parameter(torch.rand(cfg.block_size, cfg.d_model) * 0.02)
-        self.W_Q = torch.nn.Parameter(torch.rand(cfg.vocab, cfg.d_model) * 0.02)
-        self.W_K = torch.nn.Parameter(torch.rand(cfg.vocab, cfg.d_model) * 0.02)
-        self.W_V = torch.nn.Parameter(torch.rand(cfg.vocab, cfg.d_model) * 0.02)
-        self.b_q = torch.nn.Parameter(torch.zeros(cfg.d_model))
-        self.b_v = torch.nn.Parameter(torch.zeros(cfg.d_model))
-        self.b_k = torch.nn.Parameter(torch.zeros(cfg.d_model))
+        self.W_Q = torch.nn.Parameter(torch.rand(cfg.d_model, cfg.d_heads) * 0.02)
+        self.W_K = torch.nn.Parameter(torch.rand(cfg.d_model, cfg.d_heads) * 0.02)
+        self.W_V = torch.nn.Parameter(torch.rand(cfg.vocab, cfg.d_heads) * 0.02)
+        self.b_q = torch.nn.Parameter(torch.zeros(cfg.d_heads))
+        self.b_v = torch.nn.Parameter(torch.zeros(cfg.d_heads))
+        self.b_k = torch.nn.Parameter(torch.zeros(cfg.d_heads))
         self.softmax = torch.nn.Softmax(dim=-1)
     
     def forward(self, tokens):
         '''
-        What I think is going on here is we get our tokens tensor (batches, tokens, embedding), then we get the weight matrix (vocab_len, d_model).
-        As we have a row for each possible token in the weight matrix, we pick out one value corresponding to each of our token embeddings.
-        We then need to form a tensor that has the same amount of batches, has our choice of each embedding and also has another dimension representing
-        info corresponding to the model dim
+        Here we have already embedded our tokens so they are of shape (batch, block_size, d_model). 
+        The key, query and value matrices are of shape (d_model, d_head) and they project the tokens to the d_heads number of dimensions.
+        This means that the output of the attention head is of shape (batch, block_size, dhead)
         '''
-        keys = einops.einsum(self.W_K, tokens, 'vocab modeldim, batch tokens embedding -> batch modeldim embedding ') + self.b_k
-        queries = einops.einsum(self.W_Q, tokens, 'vocab modeldim, batch tokens embedding -> batch modeldim embedding ') + self.b_q
-        kq = (keys @ queries) / self.cfg.dheads**0.5
+        keys = einops.einsum(self.W_K, tokens, 'modeldim dhead, batch tokens embedding -> batch tokens dhead') + self.b_k
+        queries = einops.einsum(self.W_Q, tokens, 'modeldim dhead, batch tokens embedding -> batch tokens dhead') + self.b_q
+        kq = (keys @ queries.swapdims(-1,-2)) / self.cfg.d_heads**0.5
         masked_kq = self.mask(kq)
-        values = einops.einsum(self.W_V, tokens,'vocab modeldim, batch tokens embedding -> batch modeldim embedding') + self.b_v
+        values = einops.einsum(self.W_V, tokens,'modeldim dhead, batch tokens embedding -> batch tokens dhead') + self.b_v
         out = masked_kq @ values
-
         return out
     
     def mask(self, scores):
@@ -61,30 +60,30 @@ class MultiHeadAttention(torch.nn.Module):
         self.stack = torch.nn.ModuleList([Head(cfg) for head in range(cfg.nheads)]) 
         self.cfg = cfg
         self.layer_norm = torch.nn.LayerNorm(cfg.block_size,device=cfg.device )
-
     def forward(self,resid):
-
-        out = resid.clone()
+        outputs = []
         for head in self.stack:
-            out += resid + head(self.layer_norm(resid))
+            outputs.append(head(self.layer_norm(resid)))
+
+        out = torch.cat(outputs, -1)
+
 
         return out
     
 class MLP(torch.nn.Module):
     def __init__(self, cfg):
         super().__init__()
-        self.W_e = torch.nn.Parameter(torch.rand(cfg.batch_size, cfg.block_size, 4 * cfg.d_model))
-        self.W_u = torch.nn.Parameter(torch.rand(cfg.batch_size, cfg.block_size, cfg.d_model))
+        self.W_e = torch.nn.Parameter(torch.rand(cfg.block_size, 4 * cfg.d_model))
+        self.W_u = torch.nn.Parameter(torch.rand(cfg.block_size, cfg.d_model))
         self.cfg = cfg
         self.layer_norm = torch.nn.LayerNorm(self.cfg.block_size, device=cfg.device)
+        self.gelu = torch.nn.GELU()
 
     def forward(self,resid):
-
         resid_norm = self.layer_norm(resid)
-        emb = einops.einsum(self.W_e, resid_norm, 'batch embedding d_mlp, batch d_model embedding -> batch embedding d_mlp')
-        gelu = torch.nn.GELU()
-        activation = gelu(emb)
-        unemb =  einops.einsum(self.W_u, activation,  'batch embedding d_model, batch embedding d_mlp -> batch embedding d_model')
+        emb = einops.einsum(self.W_e, resid_norm, 'embedding d_mlp, batch d_model embedding -> batch embedding d_mlp')
+        activation = self.gelu(emb)
+        unemb =  einops.einsum(self.W_u, activation,  'embedding d_model, batch embedding d_mlp -> batch embedding d_model')
         return unemb + resid
 
 
@@ -113,6 +112,7 @@ class Model(torch.nn.Module):
         self.W_u = torch.nn.Parameter(torch.rand(self.cfg.vocab, self.cfg.d_model) * 0.02)
         self.b_u = torch.nn.Parameter(torch.zeros(self.cfg.vocab))
         self.layer_norm = torch.nn.LayerNorm(self.cfg.block_size, device=cfg.device)
+        self.softmax = torch.nn.Softmax(dim=-1)
     def embed(self, tokens):
         # B: The weight matrix is shape vocab_len x d_model of random numbers (Which are then tweaked through back prop). 
         # By indexing this with our tokens, we are picking out the rows that correspond to the tokens we have. This forms a (batch x block_size x d_model matrix)
@@ -120,45 +120,60 @@ class Model(torch.nn.Module):
         emb = self.W_e[tokens] + self.b_e
         pos = torch.arange(tokens.shape[1], device=self.cfg.device)
         pos_emb = self.W_pos[pos]
-
         return emb + pos_emb
 
     def unembed(self, tokens):
         return self.M_u[tokens] + self.b_u
 
     def forward(self, x):
+   
         resid = self.embed(x)
         for tb in self.stack:
             resid = resid + tb(resid)
 
         resid_norm = self.layer_norm(resid)
-
         # C: Our resid maintains the same shape as it originally did after we embedded it in Model.embed()
         # We now have to matrix multiply so that we have a logit for each token, for each element in the block, for each batch
         logits = einops.einsum(self.W_u, resid_norm, 'vocab d_model, batch block_size d_model  -> batch block_size vocab') + self.b_u
         #D: Each logit corresponds to a token. Here I will greedily just take the highest value logit and return it for each
-        softmax = torch.nn.Softmax(dim=-1)
-        logits = softmax(logits)
+        logits = self.softmax(logits)
         #logit_indices = torch.argmax(logits, dim=-1)
         #return logit_indices
-
         return logits
 
     def train_time(self, x, y):
-        optimiser = torch.optim.AdamW(self.parameters(), lr= 0.1)
+        optimiser = torch.optim.AdamW(self.parameters(), lr= 6e-4)
         loss_func = torch.nn.CrossEntropyLoss()
         for step in tqdm(range(self.cfg.train_steps)):
             logits = self(x)
+
+
             optimiser.zero_grad()
             loss = loss_func(
                 logits.reshape(-1, self.cfg.vocab),
                 y.reshape(-1).long()
-                )
-            if step % 1000 == 0:
+            )
+
+            if step % 100 == 0:
                 print(f"Step {step} Loss: {loss.item()}")
             loss.backward()
-
+            for name, p in model.named_parameters():
+                if p.grad is not None:
+                    print(name, p.grad.abs().mean().item())
+                    break
             optimiser.step()
+            
+
+            '''
+            print("Post Forward: ",t1 - start)
+            print("Post Loss: ",t2 - start)
+            print("Post Backward: ",t3 - start)
+            print("Post optimiser",t4 - start)
+            '''
+           
+            
+
+
 
         print(loss.item())
 
